dcb35743c064460fb9c83af44c6030109827485a
Marcel Schwarz <marcel.schwarz@sap.com>
2023-02-08 17:51:32 +0100

    Move tests to describe block

--- test/tests/concurrency.test.js
+++ test/tests/concurrency.test.js
@@ -6,75 +6,78 @@ describe('graphql resolver concurrency', () => {
   const { axios, POST } = cds.test(path.join(__dirname, '../resources/concurrency'))
   // Prevent axios from throwing errors for non 2xx status codes
   axios.defaults.validateStatus = false
-  let _log
 
-  beforeEach(() => {
-    _log = jest.spyOn(console, 'log')
-  })
+  describe('execution order of query and mutation resolvers', () => {
+    let _log
 
-  afterEach(() => {
-    jest.clearAllMocks()
-  })
+    beforeEach(() => {
+      _log = jest.spyOn(console, 'log')
+    })
 
-  test('query resolvers should be executed in parallel', async () => {
-    const query = gql`
-      query {
-        ConcurrencyService {
-          A {
-            nodes {
-              id
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    test('query resolvers should be executed in parallel', async () => {
+      const query = gql`
+        query {
+          ConcurrencyService {
+            A {
+              nodes {
+                id
+              }
             }
-          }
-          B {
-            nodes {
-              id
+            B {
+              nodes {
+                id
+              }
             }
-          }
-          C {
-            nodes {
-              id
+            C {
+              nodes {
+                id
+              }
             }
           }
         }
-      }
-    `
-    await POST('/graphql', { query })
-    expect(_log.mock.calls[0][0]).toEqual('BEGIN READ A')
-    expect(_log.mock.calls[1][0]).toEqual('BEGIN READ B')
-    expect(_log.mock.calls[2][0]).toEqual('BEGIN READ C')
-    expect(_log.mock.calls[3][0]).toEqual('END READ B')
-    expect(_log.mock.calls[4][0]).toEqual('END READ C')
-    expect(_log.mock.calls[5][0]).toEqual('END READ A')
-  })
+      `
+      await POST('/graphql', { query })
+      expect(_log.mock.calls[0][0]).toEqual('BEGIN READ A')
+      expect(_log.mock.calls[1][0]).toEqual('BEGIN READ B')
+      expect(_log.mock.calls[2][0]).toEqual('BEGIN READ C')
+      expect(_log.mock.calls[3][0]).toEqual('END READ B')
+      expect(_log.mock.calls[4][0]).toEqual('END READ C')
+      expect(_log.mock.calls[5][0]).toEqual('END READ A')
+    })
 
-  test('mutation resolvers should be executed serially', async () => {
-    const query = gql`
-      mutation {
-        ConcurrencyService {
-          A {
-            create(input: {}) {
-              id
+    test('mutation resolvers should be executed serially', async () => {
+      const query = gql`
+        mutation {
+          ConcurrencyService {
+            A {
+              create(input: {}) {
+                id
+              }
             }
-          }
-          B {
-            create(input: {}) {
-              id
+            B {
+              create(input: {}) {
+                id
+              }
             }
-          }
-          C {
-            create(input: {}) {
-              id
+            C {
+              create(input: {}) {
+                id
+              }
             }
           }
         }
-      }
-    `
-    await POST('/graphql', { query })
-    expect(_log.mock.calls[0][0]).toEqual('BEGIN CREATE A')
-    expect(_log.mock.calls[1][0]).toEqual('END CREATE A')
-    expect(_log.mock.calls[2][0]).toEqual('BEGIN CREATE B')
-    expect(_log.mock.calls[3][0]).toEqual('END CREATE B')
-    expect(_log.mock.calls[4][0]).toEqual('BEGIN CREATE C')
-    expect(_log.mock.calls[5][0]).toEqual('END CREATE C')
+      `
+      await POST('/graphql', { query })
+      expect(_log.mock.calls[0][0]).toEqual('BEGIN CREATE A')
+      expect(_log.mock.calls[1][0]).toEqual('END CREATE A')
+      expect(_log.mock.calls[2][0]).toEqual('BEGIN CREATE B')
+      expect(_log.mock.calls[3][0]).toEqual('END CREATE B')
+      expect(_log.mock.calls[4][0]).toEqual('BEGIN CREATE C')
+      expect(_log.mock.calls[5][0]).toEqual('END CREATE C')
+    })
   })
 })
