8b431fb4c0654f709b3bf630a52bc6c3f2c49a9e
Marcel Schwarz <marcel.schwarz@sap.com>
2023-03-29 09:53:40 +0200

    Add cds.request custom handler tests for CUD

--- test/resources/request/srv/request.js
+++ test/resources/request/srv/request.js
@@ -1,6 +1,11 @@
 module.exports = srv => { 
-  srv.on('READ', 'A', req => {
+  srv.on(['CREATE', 'READ', 'UPDATE'], 'A', req => {
     const { my_header } = req.headers
     return [{ id: 'df81ea80-bbff-479a-bc25-8eb16efbfaec', my_header }]
+  })
+
+  srv.on('DELETE', 'A', req => {
+    const { my_header } = req.headers
+    return my_header ? 999 : 0
   })
 }
--- test/tests/request.test.js
+++ test/tests/request.test.js
@@ -7,27 +7,97 @@ describe('graphql - cds.request', () => {
   // Prevent axios from throwing errors for non 2xx status codes
   axios.defaults.validateStatus = false
 
-  test('HTTP headers are correctly passed to custom handlers', async () => {
-    const query = gql`
-      {
-        RequestService {
-          A {
-            nodes {
-              my_header
+  describe('HTTP headers are correctly passed to custom handlers', () => {
+    const my_header = 'my header value'
+
+    test('Create', async () => {
+      const query = gql`
+        mutation {
+          RequestService {
+            A {
+              create(input: {}) {
+                my_header
+              }
             }
           }
         }
-      }
-    `
-    const my_header = 'my header value'
-    const data = {
-      RequestService: {
-        A: {
-          nodes: [{ my_header }]
+      `
+      const data = {
+        RequestService: {
+          A: {
+            create: [{ my_header }]
+          }
         }
       }
-    }
-    const response = await POST('/graphql', { query }, { headers: { my_header } })
-    expect(response.data).toEqual({ data })
+      const response = await POST('/graphql', { query }, { headers: { my_header } })
+      expect(response.data).toEqual({ data })
+    })
+
+    test('Read', async () => {
+      const query = gql`
+        {
+          RequestService {
+            A {
+              nodes {
+                my_header
+              }
+            }
+          }
+        }
+      `
+      const data = {
+        RequestService: {
+          A: {
+            nodes: [{ my_header }]
+          }
+        }
+      }
+      const response = await POST('/graphql', { query }, { headers: { my_header } })
+      expect(response.data).toEqual({ data })
+    })
+
+    test('Update', async () => {
+      const query = gql`
+        mutation {
+          RequestService {
+            A {
+              update(filter: [], input: {}) {
+                my_header
+              }
+            }
+          }
+        }
+      `
+      const data = {
+        RequestService: {
+          A: {
+            update: [{ my_header }]
+          }
+        }
+      }
+      const response = await POST('/graphql', { query }, { headers: { my_header } })
+      expect(response.data).toEqual({ data })
+    })
+
+    test('Delete', async () => {
+      const query = gql`
+        mutation {
+          RequestService {
+            A {
+              delete(filter: [])
+            }
+          }
+        }
+      `
+      const data = {
+        RequestService: {
+          A: {
+            delete: 999
+          }
+        }
+      }
+      const response = await POST('/graphql', { query }, { headers: { my_header } })
+      expect(response.data).toEqual({ data })
+    })
   })
 })
