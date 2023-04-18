51a1b81b10c783754fc51a9251b08cfc8cdce742
Marcel Schwarz <marcel.schwarz@sap.com>
2023-04-18 17:37:56 +0200

    Add operationName and variables to object

--- index.js
+++ index.js
@@ -12,17 +12,22 @@ function CDSGraphQLAdapter (options) {
   return express.Router()
   .use (express.json()) //> required by logger below
   .use ((req,_,next)=>{
-    const query = req.body?.query || req.query.query && decodeURIComponent(req.query.query)
+    let query = req.body?.query || req.query.query && decodeURIComponent(req.query.query)
     if (!query) {
       next()
       return
     }
+    query = query.trim()
 
-    const formattedQuery = query.includes('\n') && !query.startsWith('\n') ? '\n' + query + '\n' : query
-    const operationName = req.body?.operationName && { operationName: req.body.operationName }
-    const variables = process.env.NODE_ENV !== 'production' && req.body?.variables && { variables: req.body.variables }
+    const { operationName } = req.body
+    const { variables } = process.env.NODE_ENV !== 'production' && req.body
+    // Only add properties to object that aren't undefined
+    const queryInfo = Object.fromEntries(Object.entries({ operationName, variables}).filter(([,v]) => v))
+    // If query is multiline string, add padding
+    const formattedQuery = query.includes('\n') ? `\n${query}\n` : query
 
-    LOG.info (...[req.method, operationName, formattedQuery, variables].filter(e => e))
+    // Don't log undefined values or empty objects
+    LOG.info (...[req.method, queryInfo, formattedQuery].filter(e => e && Object.keys(e).length > 0))
 
     next()
   })
