4c07f8f5aec8a4fa37c39d4d405683a41808e4aa
Marcel Schwarz <marcel.schwarz@sap.com>
2023-05-22 17:29:23 +0200

    Extract service collection from `CDSGraphQLAdapter`

--- index.js
+++ index.js
@@ -4,28 +4,27 @@ const { decodeURIComponent } = cds.utils
 const LOG = cds.log('graphql')
 const express = require ('express')
 
-let services
-
-function CDSGraphQLAdapter (srv, options) {
+function CDSGraphQLAdapter (services, options) {
   const defaults = { graphiql: true }
   options = { ...defaults, ...options }
 
+  return express.Router()
+    .use (express.json()) //> required by logger below
+    .use ((req,_,next)=>{
+      LOG.info (req.method, req.body?.query || decodeURIComponent(req.query.query))
+      next()
+    })
+    .use (new GraphQLAdapter (services, options))
+}
+
+let services
+
+module.exports = (srv, options) => {
   if (!services) {
     services = {}
-    cds.on('served', ()=>{
-      cds.app.use ('/graphql',
-      cds.middlewares.before,
-      express.Router()
-        .use (express.json()) //> required by logger below
-        .use ((req,_,next)=>{
-          LOG.info (req.method, req.body?.query || decodeURIComponent(req.query.query))
-          next()
-        })
-        .use (new GraphQLAdapter (services, options)),
-      cds.middlewares.after)
+    cds.on('served', () => {
+      cds.app.use ('/graphql', cds.middlewares.before, CDSGraphQLAdapter(services, options), cds.middlewares.after)
     })
   }
-  services[srv.path] = srv
- }
-
-module.exports = CDSGraphQLAdapter
+  services[srv.name] = srv
+}
