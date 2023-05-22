96cac52c8f7b56cdca07a3e85ec40189745b5869
Marcel Schwarz <marcel.schwarz@sap.com>
2023-05-22 17:11:26 +0200

    Collect services and init adapter on served

--- index.js
+++ index.js
@@ -4,18 +4,28 @@ const { decodeURIComponent } = cds.utils
 const LOG = cds.log('graphql')
 const express = require ('express')
 
-function CDSGraphQLAdapter (options) {
-  const {services} = options
+let services
+
+function CDSGraphQLAdapter (srv, options) {
   const defaults = { graphiql: true }
   options = { ...defaults, ...options }
 
-  return express.Router()
-  .use (express.json()) //> required by logger below
-  .use ((req,_,next)=>{
-    LOG.info (req.method, req.body?.query || decodeURIComponent(req.query.query))
-    next()
-  })
-  .use (new GraphQLAdapter (services, options))
-}
+  if (!services) {
+    services = {}
+    cds.on('served', ()=>{
+      cds.app.use ('/graphql',
+      cds.middlewares.before,
+      express.Router()
+        .use (express.json()) //> required by logger below
+        .use ((req,_,next)=>{
+          LOG.info (req.method, req.body?.query || decodeURIComponent(req.query.query))
+          next()
+        })
+        .use (new GraphQLAdapter (services, options)),
+      cds.middlewares.after)
+    })
+  }
+  services[srv.path] = srv
+ }
 
 module.exports = CDSGraphQLAdapter
