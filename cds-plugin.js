eca71483f57e6e6356a54f1c9a804d4d9c0a83cd
Marcel Schwarz <marcel.schwarz@sap.com>
2023-05-22 15:24:30 +0200

    Rename protocols `path` config to `endpoint`

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,5 +1,5 @@
 const cds = require('@sap/cds')
 const protocols = cds.env.protocols ??= {}
 if (!protocols.graphql) protocols.graphql = {
-  path: "/graphql", impl: "@cap-js/graphql"
+  endpoint: "/graphql", impl: "@cap-js/graphql"
 }
