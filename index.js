0dbaa75d6bb68842bb8a3d021b11bc7c81a73ca6
Arley Triana Morin <arley.triana.morin@sap.com>
2023-05-24 15:33:55 +0200

    refactor: streamline routes

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const GraphQLAdapter = require('./lib')
-const cds = require ('@sap/cds')
+const cds = require('@sap/cds')
 const { decodeURIComponent } = cds.utils
 const LOG = cds.log('graphql')
-const express = require ('express')
+const express = require('express')
+const { createHandler } = require('graphql-http/lib/use/express')
+const { generateSchema4 } = require('./lib/schema')
+const graphiql = require('./app/graphiql')
 
-function CDSGraphQLAdapter (options) {
-  const {services} = options
+function CDSGraphQLAdapter(options) {
+  const router = express.Router()
+  const { services } = options
   const defaults = { graphiql: true }
   options = { ...defaults, ...options }
 
-  return express.Router()
-  .use (express.json()) //> required by logger below
-  .use ((req,_,next)=>{
-    LOG.info (req.method, req.body?.query || decodeURIComponent(req.query.query))
-    next()
-  })
-  .use (new GraphQLAdapter (services, options))
+  router
+    .use(express.json()) //> required by logger below
+    .use((req, _, next) => {
+      LOG.info(req.method, req.body?.query || decodeURIComponent(req.query.query))
+      next()
+    })
+
+  if (options.graphiql) router.get('/', graphiql)
+  const schema = generateSchema4(services)
+  router.use((req, res) => createHandler({ schema, context: { req, res }, ...options })(req, res))
+  return router
 }
 
 module.exports = CDSGraphQLAdapter
