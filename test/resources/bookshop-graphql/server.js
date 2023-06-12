ee982f5007def2cfd171fc4713a147fe6db07a96
Marcel Schwarz <marcel.schwarz@sap.com>
2023-06-12 13:58:40 +0200

    Revert "Rename protocols `path` config to `endpoint`"
    This reverts commit eca71483f57e6e6356a54f1c9a804d4d9c0a83cd.

--- test/resources/bookshop-graphql/server.js
+++ test/resources/bookshop-graphql/server.js
@@ -2,5 +2,5 @@ const cds = require('@sap/cds')
 const path = require('path')
 
 cds.env.protocols = {
-  graphql: { endpoint: '/graphql', impl: path.join(__dirname, '../../../index.js') }
+  graphql: { path: '/graphql', impl: path.join(__dirname, '../../../index.js') }
 }
