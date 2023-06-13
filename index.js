99c46cdfd0738d3381106efe0ee3800d866ef689
Marcel Schwarz <marcel.schwarz@sap.com>
2023-06-13 14:14:20 +0200

    Log serving protocol and path equivalent to cds 6

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const LOG = cds.log()
 const GraphQLAdapter = require('./lib/GraphQLAdapter')
 
 let services
@@ -7,6 +8,7 @@ module.exports = (srv, options) => {
     cds.on('served', () => {
       options.services = services
       cds.app.use (options.path, cds.middlewares.before, GraphQLAdapter(options), cds.middlewares.after)
+      LOG.info('serving', { protocol: 'graphql', at: options.path })
     })
   }
   services[srv.name] = srv
