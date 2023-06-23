be8e3b89ce96fb5eb2074f2938342792d0aa599d
Marcel Schwarz <marcel.schwarz@sap.com>
2023-06-23 09:33:29 +0200

    Fix configured graphql path in tests

--- test/resources/custom-handlers/server.js
+++ test/resources/custom-handlers/server.js
@@ -2,5 +2,5 @@ const cds = require('@sap/cds')
 const path = require('path')
 const protocols = cds.env.protocols ??= {}
 if (!protocols.graphql) protocols.graphql = {
-  path: '/custom-graphql-path', impl: path.join(__dirname, '../../../index.js')
+  path: '/graphql', impl: path.join(__dirname, '../../../index.js')
 }
