6db047ae741dd7d9b87200b9fa1179f96a5b99a9
Marcel Schwarz <marcel.schwarz@sap.com>
2023-08-23 15:47:49 +0200

    Extract `cds.compile.to.(gql|graphql)` registration

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,9 +1,6 @@
 const cds = require('@sap/cds')
+require('./lib/cli')
 const protocols = cds.env.protocols ??= {}
 if (!protocols.graphql) protocols.graphql = {
   path: "/graphql", impl: "@cap-js/graphql"
 }
-Object.assign(cds.compile.to, {
-  get gql() { return super.gql = require('./lib/compile') },
-  get graphql() { return super.graphql = require('./lib/compile') }
-})
