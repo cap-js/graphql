939933a4296d4e6697f00033564b99ba51594696
Marcel Schwarz <marcel.schwarz@sap.com>
2023-08-23 16:09:32 +0200

    Simplify generate-schemas script

--- test/scripts/generate-schemas.js
+++ test/scripts/generate-schemas.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 const path = require('path')
 const fs = require('fs')
-const { SCHEMAS_DIR, cdsFilesToGQLSchema, formatSchema } = require('../util')
+const { SCHEMAS_DIR, formatSchema } = require('../util')
 const { models } = require('../resources')
-const { printSchema } = require('graphql')
+const compile = require('../../lib/compile')
 
 ;(async () => {
   fs.rmSync(SCHEMAS_DIR, { recursive: true, force: true })
   fs.mkdirSync(SCHEMAS_DIR)
   for (const model of models) {
     console.log(`Generating GraphQL schema "${model.name}.gql"`)
-    const graphQLSchema = formatSchema(printSchema(await cdsFilesToGQLSchema(model.files)))
+    const graphQLSchema = formatSchema(compile(await cds.load(model.files)))
     const schemaPath = path.join(SCHEMAS_DIR, `${model.name}.gql`)
     const schemaPathDir = path.parse(schemaPath).dir
     if (!fs.existsSync(schemaPathDir)) fs.mkdirSync(schemaPathDir)
