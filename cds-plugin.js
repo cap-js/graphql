5174085bd0bd3063eda068a9d626895518501994
Marcel Schwarz <marcel.schwarz@sap.com>
2023-08-23 15:12:50 +0200

    Move compile to gql/graphql to module from @sap/cds

--- cds-plugin.js
+++ cds-plugin.js
@@ -3,3 +3,7 @@ const protocols = cds.env.protocols ??= {}
 if (!protocols.graphql) protocols.graphql = {
   path: "/graphql", impl: "@cap-js/graphql"
 }
+Object.assign(cds.compile.to, {
+  get gql() { return super.gql = require('./lib/compile') },
+  get graphql() { return super.graphql = require('./lib/compile') }
+})
