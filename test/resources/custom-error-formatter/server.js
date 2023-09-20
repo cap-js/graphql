6b63221f51f804668b592e9df1381e9a73936834
Marcel Schwarz <marcel.schwarz@sap.com>
2023-09-20 16:33:41 +0200

    More readable test project server.js

--- test/resources/custom-error-formatter/server.js
+++ test/resources/custom-error-formatter/server.js
@@ -2,5 +2,7 @@ const cds = require('@sap/cds')
 const path = require('path')
 const protocols = cds.env.protocols ??= {}
 if (!protocols.graphql) protocols.graphql = {
-  path: '/graphql', impl: path.join(__dirname, '../../../index.js'), errorFormatterImpl: path.join(__dirname, 'customErrorFormatter')
+  path: '/graphql',
+  impl: path.join(__dirname, '../../../index.js'),
+  errorFormatterImpl: path.join(__dirname, 'customErrorFormatter')
 }
