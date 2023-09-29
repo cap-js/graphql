678de57e0b35360a2b3392d315cbef99315eeb0b
Marcel Schwarz <marcel.schwarz@sap.com>
2023-09-29 11:37:21 +0200

    Improve lazy loading and clarify purpose of api module

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,5 +1,5 @@
 const cds = require('@sap/cds')
-require('./lib/api')
+require('./lib/api').registerCompileTargets(cds.compile.to)
 const protocols = cds.env.protocols ??= {}
 if (!protocols.graphql) protocols.graphql = {
   path: "/graphql", impl: "@cap-js/graphql"
