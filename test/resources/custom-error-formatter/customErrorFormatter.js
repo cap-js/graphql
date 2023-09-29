e7e8a4d37c134f1735b188c7ac05528daf382933
Marcel Schwarz <marcel.schwarz@sap.com>
2023-09-29 15:34:35 +0200

    Rename err -> error in custom error formatter

--- test/resources/custom-error-formatter/customErrorFormatter.js
+++ test/resources/custom-error-formatter/customErrorFormatter.js
@@ -1,10 +1,10 @@
 let _count = 0
 
-module.exports = err => {
-  const message = 'Oops! ' + err.message
+module.exports = error => {
+  const message = 'Oops! ' + error.message
   const custom = 'This property is added by the custom error formatter'
   const count = _count++
-  const details = err.details // Exists if this is an outer error of multiple errors
+  const details = error.details // Exists if this is an outer error of multiple errors
 
   // Return a copy of the error with the desired formatting
   return { message, custom, count, details}
