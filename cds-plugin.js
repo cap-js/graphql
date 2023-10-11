c0ff69941d4875a6e4fdcb481561b3df35376b81
Marcel Schwarz <marcel.schwarz@sap.com>
2023-10-11 11:27:04 +0200

    Move `cds.compile.to` into register function

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,5 +1,5 @@
 const cds = require('@sap/cds')
-require('./lib/api').registerCompileTargets(cds.compile.to)
+require('./lib/api').registerCompileTargets()
 const protocols = cds.env.protocols ??= {}
 if (!protocols.graphql) protocols.graphql = {
   path: "/graphql", impl: "@cap-js/graphql"
