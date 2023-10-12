5dc882f51b89c3729aea7c016bed1694793c97cd
Marcel Schwarz <marcel.schwarz@sap.com>
2023-10-12 12:44:57 +0200

    Single quotes instead of double quotes

--- cds-plugin.js
+++ cds-plugin.js
@@ -1,6 +1,6 @@
 const cds = require('@sap/cds')
 require('./lib/api').registerCompileTargets()
-const defaults = { path: "/graphql", impl: "@cap-js/graphql" }
+const defaults = { path: '/graphql', impl: '@cap-js/graphql' }
 const protocols = cds.env.protocols ??= {}
 protocols.graphql ??= {}
 protocols.graphql = { ...defaults, ...protocols.graphql}
