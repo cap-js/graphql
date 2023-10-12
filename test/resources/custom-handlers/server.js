e5530568e05a9932fc49dedbbe9a8b5e23cd8c10
Marcel Schwarz <marcel.schwarz@sap.com>
2023-10-12 15:11:29 +0200

    Simpler programmatic config that points to module name

--- test/resources/custom-handlers/server.js
+++ test/resources/custom-handlers/server.js
@@ -1,7 +1,4 @@
 const cds = require('@sap/cds')
-const path = require('path')
 // Programmatic configuration of GraphQL protocol adapter
 const protocols = cds.env.protocols ??= {}
-if (!protocols.graphql) protocols.graphql = {
-  path: '/graphql', impl: path.join(__dirname, '../../../index.js')
-}
+protocols.graphql = { path: '/graphql', impl: '@cap-js/graphql' }
