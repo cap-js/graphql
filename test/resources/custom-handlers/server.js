46c30605611d589d860c6808e8dd1d3a8b084cb7
Marcel Schwarz <marcel.schwarz@sap.com>
2023-10-12 12:12:58 +0200

    Replace more programmatic with declarative configs

--- test/resources/custom-handlers/server.js
+++ test/resources/custom-handlers/server.js
@@ -1,5 +1,6 @@
 const cds = require('@sap/cds')
 const path = require('path')
+// Programmatic configuration of GraphQL protocol adapter
 const protocols = cds.env.protocols ??= {}
 if (!protocols.graphql) protocols.graphql = {
   path: '/graphql', impl: path.join(__dirname, '../../../index.js')
