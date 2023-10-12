13a7c8dab5b976a63be7bd3ff6e5033ab49a13d7
Marcel Schwarz <marcel.schwarz@sap.com>
2023-10-12 12:08:10 +0200

    Configure protocol path in test project `package.json`

--- test/resources/annotations/server.js
+++
@@ -1,5 +0,0 @@
-const cds = require('@sap/cds')
-const protocols = cds.env.protocols ??= {}
-if (!protocols.graphql) protocols.graphql = {
-  path: '/custom-graphql-path', impl: '@cap-js/graphql'
-}
