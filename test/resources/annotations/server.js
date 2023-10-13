eecc08bf2b409ed68aca8cbcdf1f940654212f0b
Marcel Schwarz <marcel.schwarz@sap.com>
2023-10-13 12:46:08 +0200

    fix: `errorFormatter` relative to project root and improved merging of protocol config  (#129)
    * Create a symlink to self to enable test projects to require `@cap-js/graphql`
    
    * Revert "Create a symlink to self to enable test projects to require `@cap-js/graphql`"
    
    This reverts commit 60d7d0f94c25942678dcd7c80b491aaf0eab5fa8.
    
    * Add dev dependency to self
    
    * Re-add self requires in tests
    
    * Configure protocol path in test project `package.json`
    
    * Improve merging of protocol configuration with plugin defaults
    
    * Replace more programmatic with declarative configs
    
    * Load custom `errorFormatter` relative to CDS project root
    
    * Single quotes instead of double quotes
    
    * Remove `impl` from config
    
    * Improve readability of loading error formatter
    
    * Another declarative instead of programmatic config
    
    * Simpler programmatic config that points to module name
    
    * Reorder imports

--- test/resources/annotations/server.js
+++
@@ -1,6 +0,0 @@
-const cds = require('@sap/cds')
-const path = require('path')
-const protocols = cds.env.protocols ??= {}
-if (!protocols.graphql) protocols.graphql = {
-  path: '/custom-graphql-path', impl: path.join(__dirname, '../../../index.js')
-}
--- test/resources/custom-error-formatter/server.js
+++
@@ -1,8 +0,0 @@
-const cds = require('@sap/cds')
-const path = require('path')
-const protocols = cds.env.protocols ??= {}
-if (!protocols.graphql) protocols.graphql = {
-  path: '/graphql',
-  impl: path.join(__dirname, '../../../index.js'),
-  errorFormatter: path.join(__dirname, 'customErrorFormatter')
-}
