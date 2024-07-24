b8377fe3f7e4f303dff670dc82f17909652996ca
Marcel Schwarz <marcel.schwarz@sap.com>
2024-07-24 15:55:49 +0200

    Replace programmatic body parser limit override with global config

--- test/resources/types/server.js
+++
@@ -1,4 +0,0 @@
-cds.on('bootstrap', app => {
-  // Programmatic override of maximum request body size for requests to GraphQL endpoint
-  app.use(cds.env.protocols.graphql.path, require('express').json({ limit: '110KB' }))
-})
