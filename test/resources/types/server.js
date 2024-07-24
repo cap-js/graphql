a28d788499927c0937e3f17940e06d50ae2c4516
Marcel Schwarz <marcel.schwarz@sap.com>
2024-07-24 13:44:28 +0200

    Add comment about programmatic body limit override

--- test/resources/types/server.js
+++ test/resources/types/server.js
@@ -1,3 +1,4 @@
 cds.on('bootstrap', app => {
+  // Programmatic override of maximum request body size for requests to GraphQL endpoint
   app.use(cds.env.protocols.graphql.path, require('express').json({ limit: '110KB' }))
 })
