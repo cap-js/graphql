1c5f2079d540bf601c442c8d79888bd0a03e697a
Marcel Schwarz <marcel.schwarz@sap.com>
2024-07-30 14:10:26 +0200

    test: unskip `Int64` tests with `ieee754compatible` flag enabled (#180)

--- test/tests/types.test.js
+++ test/tests/types.test.js
@@ -803,7 +803,7 @@ describe('graphql - types parsing and validation', () => {
   })
 
   // Note: maps to same type as cds.Integer64
-  xdescribe('cds.Int64', () => {
+  describe('cds.Int64', () => {
     const field = 'myInt64'
 
     test('cds.Int64 is correctly parsed from input literal int value', async () => {
@@ -854,7 +854,7 @@ describe('graphql - types parsing and validation', () => {
     })
   })
 
-  xdescribe('cds.Integer64', () => {
+  describe('cds.Integer64', () => {
     const field = 'myInteger64'
 
     describe('input literal', () => {
