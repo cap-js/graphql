4b6f8c3669e47707b5566dd8b76b1d8be1e01ae0
heikowitteborg <heiko@witteb.org>
2025-03-18 14:32:11 +0100

    Update concurrency.test.js

--- test/tests/concurrency.test.js
+++ test/tests/concurrency.test.js
@@ -4,7 +4,7 @@ describe('graphql - resolver concurrency', () => {
   const { gql } = require('../util')
 
 
-  // bla
+  // blabla
   const { axios, POST } = cds.test(path.join(__dirname, '../resources/concurrency'))
   // Prevent axios from throwing errors for non 2xx status codes
   axios.defaults.validateStatus = false
