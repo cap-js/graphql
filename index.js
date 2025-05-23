e13e8e1a7fec807889af036c6c05e6e3894d5cbf
Marcel Schwarz <marcel.schwarz@sap.com>
2025-05-23 15:24:34 +0200

    Add `ws` and `graphql-ws`

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const cds = require('@sap/cds')
 const DEBUG = cds.debug('adapters')
 const GraphQLAdapter = require('./lib/GraphQLAdapter')
+const { WebSocketServer } = require('ws')
 
 let services
 const collectServicesAndMountAdapter = (srv, options) => {
   if (!services) {
     services = {}
+    const wss = new WebSocketServer({ noServer: true })
+    options.wss = wss
     cds.on('served', () => {
       options.services = services
-      cds.app.use (options.path, cds.middlewares.before, GraphQLAdapter(options), cds.middlewares.after)
+      cds.app.use(options.path, cds.middlewares.before, GraphQLAdapter(options), cds.middlewares.after)
       DEBUG?.('app.use(', options.path, ', ... )')
+    })
+    cds.on('listening', app => {
+      const { server } = app
+      server.on('upgrade', (request, socket, head) => {
+        wss.handleUpgrade(request, socket, head, ws => {
+          wss.emit('connection', ws, request)
+        })
+      })
     })
   }
   services[srv.name] = srv
