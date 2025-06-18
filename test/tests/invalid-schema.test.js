d9ce5cb645c0f68c94679236e56264033f23311f
Marcel Schwarz <marcel.schwarz@sap.com>
2025-06-18 11:21:21 +0200

    Add additional describe block to test suite

--- test/tests/invalid-schema.test.js
+++ test/tests/invalid-schema.test.js
@@ -4,25 +4,27 @@ require('../../cds-plugin')
 
 const RES = path.join(__dirname, '../resources')
 
-describe('graphql - schema generation fails due to incompatibility with modelling', () => {
-  it('special characters in service name', async () => {
-    const csn = await cds.load(RES + '/special-chars/srv/service')
-    expect(() => {
-      cds.compile(csn).to.graphql()
-    }).toThrow(/SpecialCharsÄÖÜService/)
-  })
+describe('graphql - schema generation fails due to incompatible modelling', () => {
+  describe('special characters', () => {
+    it('in service name', async () => {
+      const csn = await cds.load(RES + '/special-chars/srv/service')
+      expect(() => {
+        cds.compile(csn).to.graphql()
+      }).toThrow(/SpecialCharsÄÖÜService/)
+    })
 
-  it('special characters in entity name', async () => {
-    const csn = await cds.load(RES + '/special-chars/srv/entity')
-    expect(() => {
-      cds.compile(csn).to.graphql()
-    }).toThrow(/RootÄÖÜEntity/)
-  })
+    it('in entity name', async () => {
+      const csn = await cds.load(RES + '/special-chars/srv/entity')
+      expect(() => {
+        cds.compile(csn).to.graphql()
+      }).toThrow(/RootÄÖÜEntity/)
+    })
 
-  it('special characters in element name', async () => {
-    const csn = await cds.load(RES + '/special-chars/srv/element')
-    expect(() => {
-      cds.compile(csn).to.graphql()
-    }).toThrow(/myÄÖÜElement/)
+    it('in element name', async () => {
+      const csn = await cds.load(RES + '/special-chars/srv/element')
+      expect(() => {
+        cds.compile(csn).to.graphql()
+      }).toThrow(/myÄÖÜElement/)
+    })
   })
 })
