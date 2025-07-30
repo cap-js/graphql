f5e903411b0f186a97146eadabd5ffa2e32f96a1
Marcel Schwarz <marcel.schwarz@sap.com>
2025-07-30 14:46:32 +0200

    Formatting

--- app/graphiql.js
+++ app/graphiql.js
@@ -6,12 +6,9 @@ const fs = require('node:fs')
 const html = fs.readFileSync(path.join(__dirname, 'graphiql.html'))
 
 router.get('/', async (req, res, next) => {
-  if (req.query.query) {
-    // Forward GET requests with query URL parameter to GraphQL server
-    next()
-  } else {
-    res.type('html').send(html)
-  }
+  // Forward GET requests with query URL parameter to GraphQL server
+  if (req.query.query) next()
+  else res.type('html').send(html)
 })
 
 module.exports = router
