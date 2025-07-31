70cf44ab01497082417bcc2115f028ad356aa1bc
Marcel Schwarz <marcel.schwarz@sap.com>
2025-07-31 11:40:45 +0200

    Rm `async` from GraphiQL `GET` handler

--- app/graphiql.js
+++ app/graphiql.js
@@ -5,7 +5,7 @@ const fs = require('node:fs')
 
 const html = fs.readFileSync(path.join(__dirname, 'graphiql.html'))
 
-router.get('/', async (req, res, next) => {
+router.get('/', (req, res, next) => {
   // Forward GET requests with query URL parameter to GraphQL server
   if (req.query.query) next()
   else res.type('html').send(html)
