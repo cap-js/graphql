06170f629f58698d5c1afe8f26e94509d5438e27
Marcel Schwarz <marcel.schwarz@sap.com>
2025-08-08 14:15:39 +0200

    WIP: lazy load HTML file async on first request

--- app/graphiql.js
+++ app/graphiql.js
@@ -1,14 +1,25 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
-const fs = require('node:fs')
+const { readFile } = require('node:fs/promises')
 
-const html = fs.readFileSync(path.join(__dirname, 'graphiql.html'))
+let html
 
-router.get('/', (req, res, next) => {
+const graphiql = async next => {
+  if (!html) {
+    try {
+      html = await readFile(path.join(__dirname, 'graphiql.html'))
+    } catch (e) {
+      next(new Error(`Failed to read GraphiQL HTML from file`, { cause: e }))
+    }
+  }
+  return html
+}
+
+router.get('/', async (req, res, next) => {
   // Forward GET requests with query URL parameter to GraphQL server
-  if (req.query.query) next()
-  else res.type('html').send(html)
+  if (req.query.query) return next()
+  res.type('html').send(await graphiql(next))
 })
 
 module.exports = router
