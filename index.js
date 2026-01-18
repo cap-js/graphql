5b60b825e3210b0a3702be95aafcbdbf3170e17f
Lochner Louw <3734918+lochlouw@users.noreply.github.com>
2026-01-18 13:43:10 +1000

    feat: add GraphiQL link to service index                                                                                                                                                                                        - Registers a link provider that adds a GraphiQL link to the service index HTML page for GraphQL endpoints, making it easier to access the GraphiQL interface.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const cds = require('@sap/cds')
 const DEBUG = cds.debug('adapters')
 const GraphQLAdapter = require('./lib/GraphQLAdapter')
+const { addLinkToIndexHtml } = require('./lib/utils')
 
 let services
 const collectServicesAndMountAdapter = (srv, options) => {
   if (!services) {
     services = {}
+
+    cds.on('serving', service => {
+      // Add link to GraphiQL in service index HTML
+      addLinkToIndexHtml(service)
+    })
+
     cds.on('served', () => {
       options.services = services
       cds.app.use (options.path, cds.middlewares.before, GraphQLAdapter(options), cds.middlewares.after)
